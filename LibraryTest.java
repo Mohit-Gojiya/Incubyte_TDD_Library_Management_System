3a9fe9e20a76b38273eaf28c83251c26d5cc37a5
Mohit-Gojiya <gojiyamohit007@gmail.com>
2024-08-31 13:06:54 +0530

    Testcase : LibraryName Should Not Be Null
    Testcase : test_LibraryName_ShouldNot_beNull
    
    Failed

--- LibraryTest.java
+++ LibraryTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
+
+    @Test
+    public void test_LibraryName_ShouldNot_BeNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Library(null));
+    }
 
 }
