12338ab8cf58eca84331f19ed43dcf6f4aba1c66
Mohit-Gojiya <gojiyamohit007@gmail.com>
2024-08-31 13:14:28 +0530

    Testcase Passed : LibraryName Should Not Be Null
    Created Library constructor with name not null validation

--- Library.java
+++ Library.java
@@ -1,2 +1,10 @@
 public class Library {
+
+    private final String Name;
+
+    public Library (String Name)
+    {
+        if (Name == null) { throw new IllegalArgumentException("Library Name Should not be null"); }
+        this.Name = Name;
+    }
 }
