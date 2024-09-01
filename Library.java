4692e9ca179e36eb32502bc28325993a36bbd4f5
Mohit-Gojiya <gojiyamohit007@gmail.com>
2024-09-01 10:33:30 +0530

    [Refactor]

--- Library.java
+++ Library.java
@@ -1,21 +1,21 @@
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Library {
 
     String Name;
-
     private final Map<String, Book> Book_Inventory;
     private final Map<String, User> User_Catalog;
     private final Map<String, String> BorrowedBooks;
     private final Map<String, Book> BorrowedBookDetails;
 
+    public Library(String Name)
+    {
+        if (Name == null || Name.isBlank() || Name.length() <= 3)
+        { throw new IllegalArgumentException("Library Name Should not be null"); }
 
-    public Library(String Name) {
-        if (Name == null || Name.isEmpty() || Name.length() <= 3) {
-            throw new IllegalArgumentException("Library Name Should not be null");
-        }
         this.Name = Name;
         this.Book_Inventory = new HashMap<String, Book>();
         this.User_Catalog = new HashMap<String, User>();
@@ -23,84 +23,54 @@ public class Library {
         this.BorrowedBookDetails = new HashMap<String, Book>();
     }
 
-    
-
-    public void addBook(User user, Book book) throws Exception {
-        if (user.isPermittedToAddBook()) {
-            Book_Inventory.put(book.getIsbn(), book);
-        } else {
-            throw new Exception("You are not authorized to add book");
-        }
+    public void addBook(User user, Book book) throws Exception
+    {
+        if (book == null) { throw new IllegalArgumentException("Book Null"); }
+        if (user.isPermittedToAddBook()) { Book_Inventory.put(book.getIsbn(), book); }
+        else { throw new Exception("You are not authorized to add book"); }
     }
 
+    public Book get_Isbn_from_Library_Inventory(String Isbn) { return Book_Inventory.get(Isbn); }
 
-
-    public Book get_Isbn_from_Library_Inventory(String Isbn) {
-        return Book_Inventory.get(Isbn);
-    }
-
-
-
-    public void addUser(User user) {
-        if (User_Catalog.containsKey(user.getUserName())) {
-            throw new IllegalArgumentException("User already exists in catalog");
-        }
+    public void addUser(User user)
+    {
+        if (User_Catalog.containsKey(user.getUserName()))
+        { throw new IllegalArgumentException("User already exists in catalog"); }
         User_Catalog.put(user.getUserName(), user);
     }
 
+    public User getUserByName(String username) { return User_Catalog.get(username); }
 
+    public Map<String, Book> viewAvailableBooks() { return Collections.unmodifiableMap(new HashMap<>(Book_Inventory)); }
 
-    public User getUserByName(String username) {
-        return User_Catalog.get(username);
-    }
-
-
-
-    public Map<String, Book> viewAvailableBooks() {
-        return Collections.unmodifiableMap(new HashMap<>(Book_Inventory));
-    }
-
-
-
-    public void borrowBook(User user, String Isbn) {
-
+    public void borrowBook(User user, String Isbn)
+    {
         Book book = Book_Inventory.get(Isbn);
-        if (BorrowedBooks.containsKey(Isbn)) {
-            throw new IllegalArgumentException("Book is already borrowed");
-        }
-        if (book != null) {
+        if (BorrowedBooks.containsKey(Isbn)) { throw new IllegalArgumentException("Book is already borrowed"); }
+        if (book != null)
+        {
             BorrowedBooks.put(Isbn, user.getUserName());
             BorrowedBookDetails.put(Isbn, book);
             Book_Inventory.remove(Isbn);
-        } else throw new IllegalArgumentException("Book not found");
-
+        }
+        else throw new IllegalArgumentException("Book not found");
     }
 
+    public Book getBookByISBN(String Isbn) { return Book_Inventory.get(Isbn); }
 
+    public String getBorrowerNameByISBN(String Isbn) { return BorrowedBooks.get(Isbn); }
 
-    public Book getBookByISBN(String Isbn) {
-        return Book_Inventory.get(Isbn);
-    }
-
-
-
-    public String getBorrowerNameByISBN(String Isbn) {
-        return BorrowedBooks.get(Isbn);
-    }
-
-    public void returnBook(User user, String Isbn) {
+    public void returnBook(User user, String Isbn)
+    {
         Book book = BorrowedBookDetails.get(Isbn);
+        if (!BorrowedBooks.containsKey(Isbn)) { throw  new IllegalArgumentException("Book was not borrowed by any user");}
 
-        if (BorrowedBooks.containsKey(Isbn))
-        {
-            if (user.getUserName() == BorrowedBooks.get(Isbn))
+        if (Objects.equals(user.getUserName(), BorrowedBooks.get(Isbn)))
             {
                 Book_Inventory.put(Isbn, book);
                 BorrowedBooks.remove(Isbn);
                 BorrowedBookDetails.remove(Isbn);
             }
-            else throw new IllegalArgumentException("book was not borrowed by this user");
-        }
-        else throw  new IllegalArgumentException("Book was not borrowed by any user");
+        else throw new IllegalArgumentException("book was not borrowed by this user");
     }
 }
